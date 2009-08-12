0000916b5dca1cded96d80067108587f67c4170a
id:ultraist <ultraist@private.udp.jp>
2009-08-12 02:21:11 +0000

    sim

--- contest01.pl
+++ contest01.pl
@@ -5,7 +5,7 @@ $| = 1;
 
 sub read_repo
 {
-  my %repo;
+  my $repo = {};
   my @conv;
   my $i = 0;
 
@@ -16,19 +16,19 @@ sub read_repo
   while (my $line = <R>) {
     chomp($line);
     my ($repo_id, undef) = split(":", $line);
-    $repo{$repo_id} = 0.0;
+    $repo->{$repo_id} = 0.0;
     ++$i;
   }
   close(R);
   printf("read repo %d\n", $i);
   
-  return { id => \%repo, n => $i };
+  return { id => $repo, n => $i };
 }
 
 sub read_user
 {
   my $repo = shift;
-  my %user;
+  my $user = {};
   my $i = 0;
   
   print "read users\r";
@@ -38,45 +38,45 @@ sub read_user
   while (my $line = <U>) {
     chomp($line);
     my ($user_id, $repo_id) = split(":", $line);
-    if (!exists($user{$user_id})) {
-      $user{$user_id} = {};
-      $user{$user_id}->{$repo_id} = 1;
+    if (!exists($user->{$user_id})) {
+      $user->{$user_id} = {};
+      $user->{$user_id}->{$repo_id} = 1;
     } else {
-      $user{$user_id}->{$repo_id} = 1;
+      $user->{$user_id}->{$repo_id} = 1;
     }
   }
   close(U);
   
-  my %sample_user;
-  my $count = scalar(keys(%user));
+  my $sample_user = {};
+  my $count = scalar(keys(%$user));
   my $avg = 0.0;
   my $var = 0.0;
   my $sd = 0.0;
   my $samples = 0;
-  foreach my $k (keys(%user)) {
-    my $p = scalar(keys(%{$user{$k}}));
+  foreach my $k (keys(%$user)) {
+    my $p = scalar(keys(%{$user->{$k}}));
     $avg += $p / $count;
   }
-  foreach my $k (keys(%user)) {
-    my $p = scalar(keys(%{$user{$k}}));
+  foreach my $k (keys(%$user)) {
+    my $p = scalar(keys(%{$user->{$k}}));
     $var += ($p - $avg) ** 2 / ($count - 1);
   }
   $sd = sqrt($var);
-  foreach my $k (keys(%user)) {
-    my $p = scalar(keys(%{$user{$k}}));
+  foreach my $k (keys(%$user)) {
+    my $p = scalar(keys(%{$user->{$k}}));
     if ($avg / 2 < $p && $p < $avg + $sd) {
-      $sample_user{$k} = $user{$k};
+      $sample_user->{$k} = $user->{$k};
     }
   }
-  $samples =  scalar(keys(%sample_user));
+  $samples =  scalar(keys(%$sample_user));
   printf("read user , count: %d, var:%f, sd:%f, samples:%d\n", $count, $var, $sd, $samples);
   
-  return { id => \%sample_user, all_id => \%user, n => $samples};
+  return { id => $sample_user, all_id => $user, n => $samples};
 }
 
 sub read_test
 {
-  my @uid;
+  my $uid = [];
   open(T, "download/test.txt") or die $!;
   
   print "read test\r";
@@ -84,10 +84,10 @@ sub read_test
   while (my $line = <T>) {
     chomp($line);
     my $user_id = $line;
-    push(@uid, $user_id);
+    push(@$uid, $user_id);
   }
   close(T);
-  return \@uid;
+  return $uid;
 }
 
 sub repo_rank
@@ -110,28 +110,32 @@ sub repo_rank
     foreach my $i (keys(%{$repo->{id}})) {
 	$repo->{id}->{$i} *= $factor;
     }
+
+    my $rank = [];
+    foreach my $i (keys(%{$repo->{id}})) {
+	push(@$rank, { id => $i, score => $repo->{id}->{$i}});
+    }
+    @$rank = sort { $b->{score} <=> $a->{score} } @$rank;
+    $repo->{rank} = $rank;
 }
 
-sub jaccard
+sub sim
 {
   my ($v1, $v2) = @_;
-  my $ng = 0.1;
-  my $ok11 = 0.1;
+  my $n = 0;
+  my $ok11 = 0;
   
   foreach my $k (keys(%$v1)) {
     if (defined($v2->{$k})) {
       $ok11 += 1.0;
-    } else {
-      $ng += 1.0;
     }
+    ++$n;
   }
-  foreach my $k (keys(%$v2)) {
-    if (!defined($v1->{$k})) {
-      $ng += 1.0;
-    }
+  if ($n == 0) {
+      return 0;
   }
   
-  return ($ok11 + $ng) / $ok11;
+  return $ok11 / $n;
 }
 
 sub print_vec
@@ -151,41 +155,71 @@ sub recommend_repo
   my @rec_vec;
   my @dist;
   my @result;
-  my ($i, $j, $c);
+  my ($i, $j, $c, $matchs);
+  my $nfac = 1.0 / $n;
   
   foreach my $uid (keys(%{$user->{id}})) {
     my $user_vec = $user->{id}->{$uid};
-    push(@dist, { uid => $uid, dist => jaccard($vec, $user_vec)})
+    push(@dist, { uid => $uid, dist => -sim($vec, $user_vec)})
   }
   @dist = sort { $a->{dist} <=> $b->{dist} } @dist;
   #print "------------------\n";
   #print_vec($vec, $repo);
-  
+
+  $matchs = 0;
   for ($j = 0; $j < $n; ++$j) {
     my $repo_vec = $user->{id}->{$dist[$j]->{uid}};
     my $base = 0.5;
-    #if ($j < 5) {
-    #  print "$j:";
-    #  print_vec($repo_vec, $repo);
-    #}
-    foreach $i (keys(%$repo_vec)) {
-      if (!defined($score_vec{$i})) {
-        $score_vec{$i} = { i => $i, score => 0.0 };
-        $score_vec{$i}->{score} = $base + $repo->{id}->{$i};
-      } else {
-        $score_vec{$i}->{score} += $base + $repo->{id}->{$i};
-      }
+    if ($dist[$j]->{dist} != 0.0) {
+	#if ($j < 5) {
+	#  print "$j($dist[$j]->{dist}):";
+	#  print_vec($repo_vec, $repo);
+	#}
+	foreach $i (keys(%$repo_vec)) {
+	    if (!defined($score_vec{$i})) {
+		$score_vec{$i} = { i => $i, score => 0.0 };
+		$score_vec{$i}->{score} = 1.0 + $nfac * $repo->{id}->{$i};
+	    } else {
+		$score_vec{$i}->{score} += 1.0 + $nfac * $repo->{id}->{$i};
+	    }
+	}
+	++$matchs;
     }
   }
-  @rec_vec = sort { $b->{score} <=> $a->{score} } values(%score_vec);
-  $i = $c = 0;
-  while ($c < 10) {
-    if (!defined($vec->{$rec_vec[$i]->{i}})) {
-      #printf("%s:%f\n", $rec_vec[$i]->{i}, $rec_vec[$i]->{score});
-      push(@result, $rec_vec[$i]->{i});
-      ++$c;
-    }
-    ++$i;
+  
+  if ($matchs == 0) {
+      $c = 0;
+      for ($j = 0; $j < scalar(@{$repo->{rank}}); ++$j) {
+	  if (!defined($vec->{$repo->{rank}->[$j]->{id}})) {
+	      push(@result, $repo->{rank}->[$j]->{id});
+	      ++$c;
+	  }
+	  if ($c >= 10) {
+	      last;
+	  }
+      }
+  } else {
+      @rec_vec = sort { $b->{score} <=> $a->{score} } values(%score_vec);
+      $i = $c = 0;
+      while ($c < 10) {
+	  if (!defined($vec->{$rec_vec[$i]->{i}})) {
+	      #printf("%s:%f\n", $rec_vec[$i]->{i}, $rec_vec[$i]->{score});
+	      push(@result, $rec_vec[$i]->{i});
+	      ++$c;
+	  }
+	  ++$i;
+	  if (!defined($rec_vec[$i]->{i})) {
+	      for ($j = 0; $j < scalar(@{$repo->{rank}}); ++$j) {
+		  if (!defined($vec->{$repo->{rank}->[$j]->{id}})) {
+		      push(@result, $repo->{rank}->[$j]->{id});
+		      ++$c;
+		  }
+		  if ($c >= 10) {
+		      last;
+		  }
+	      }
+	  }
+      }
   }
   return @result;
 }
